a396cf11390d84428b9d9c2f80cd39b07bf2d647
anderrrs-23 <Andi.Johnson@ngc.com>
2025-10-17 19:37:47 +0000

    edits and notes for ch2 video 6

--- src/Ch02/02_06e/CodeDemo.cpp
+++ src/Ch02/02_06e/CodeDemo.cpp
@@ -2,17 +2,23 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
+// the #include is a directive part of the preprocessor directive which occurs prior to compiling
+// the words in the <> after the directive tells the compiler which file to use (the path is setup in the environment). Those in the standard library do not need an extension or specific file path
 #include <iostream>
 #include <string>
+//<cstind> is part of C standard library so it has the c at the beginning but exists in a special version in the C++ standard lib
 #include <cstdint>
 
+//#define directive used to create macros (find and replace)- only use fixed constants (general nomenclature for macros it to use an all-caps variable name)
+//e.g. the code below will replace anything called "CAPACITY" with 5000
 #define CAPACITY 5000
-#define DEBUG
+#define DEBUG //does not to be assigned to any value if being used for a ifdef directive
 
 int main(){
-    int32_t large = CAPACITY;
-    uint8_t small = 37;
-#ifdef DEBUG
+    int32_t large = CAPACITY;//signed 32 bit integer
+    uint8_t small = 37; //unsigned 8bit integer
+#ifdef DEBUG //ifdef is a conditional that work with defined directives
+//notice that it is on the same indentation line as the 'int main' and ends with '#endif'
     std::cout << "[About to perform the addition]" << std::endl;
 #endif
     large += small; // Shorthand for "large = large + small;"
