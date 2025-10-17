21590d896b3795ff35311ab05837fcbd436cb69f
anderrrs-23 <Andi.Johnson@ngc.com>
2025-10-17 15:52:02 +0000

    my attempt at first challenge no help

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -5,11 +5,28 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+//challenge hints
+//prompts input (no prompt text) and prints out input (one word input)
+//int main(){
+//    std::string str;
+//    std::cin >> user_input_name; //cin for input ONLY uses single words as input (no spaces- that is another function)
+//    std::cout << str;
+//
+//    std::cout << std::endl << std::endl;
+//    return (0);
+//}
 
+//challenge: ask for the user's name and reply
+int main(){
+    //prompt user for input
+    std::cout << "Please tell me your name: " << std::endl;
+    std::string user_input_name;
+    std::cin >> user_input_name;
+    //reply
+    std::cout << "Hello, ";
+    std::cout << user_input_name;
+    std::cout << ". Nice to meet you. I am a C++ novice.";
     std::cout << std::endl << std::endl;
     return (0);
 }
+
