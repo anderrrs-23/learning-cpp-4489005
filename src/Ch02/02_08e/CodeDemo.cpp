24d66fcd397dd596edc7d121c735fe585d6a21d6
anderrrs-23 <Andi.Johnson@ngc.com>
2025-10-22 12:32:23 +0000

    added notes for arrays

--- src/Ch02/02_08e/CodeDemo.cpp
+++ src/Ch02/02_08e/CodeDemo.cpp
@@ -2,16 +2,29 @@
 // Exercise 02_08
 // Arrays, by Eduardo Corpeño 
 
+/*
+General array notes:
+1. Collections of data- accessable through an index
+2. Homogeneous- they are all of the same data time 
+3. Fixed in size- no resizing
+4. Elements are continuous in memory (i.e. the whole array is a sequential block of memory addresses
+*/
 #include <iostream>
-
+/*about constants:
+MACROS have no typing or syntax standards for constants, so be weary of them
+const qualifier is a variable declaration, so it does have typing and syntax regulation
+      - it means that the variable cannot change
+*/ 
 // #define AGE_LENGTH 4
 
 int main(){
     const size_t AGE_LENGTH = 4;
 
     int age[AGE_LENGTH];
+    // technically the values in the array below are doubles, but the float in front makes the compiler treat them like floats
+    // they need an f after each constant to be a float
     float temperature[] = {31.5, 32.7, 38.9};
-
+    //arrays are indexed by 0!
     age[0] = 25;
     age[1] = 20;
     age[2] = 19;
