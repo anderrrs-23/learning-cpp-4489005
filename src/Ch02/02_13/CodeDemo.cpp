8580f6ada43d7d9e26dfb3ef847754d5ee50fab0
anderrrs-23 <Andi.Johnson@ngc.com>
2025-10-22 15:54:35 +0000

    ch 02 challenge

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -7,8 +7,8 @@
 int main(){
     int nums[5] = {1,23,32,24,337};
     float result; 
-
-    // Write your code here
+    // my work no help
+    result = (float(nums[0]+nums[1]+nums[2]+nums[3]+nums[4]) / 5.0);
     
     std::cout << "The average is " << result << std::endl;
     
