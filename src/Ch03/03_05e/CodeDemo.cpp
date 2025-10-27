2973afd269bb10bde59736b801512678fd0b8c0f
anderrrs-23 <Andi.Johnson@ngc.com>
2025-10-27 18:25:44 +0000

    Notes about header and Implementation files

--- src/Ch03/03_05e/CodeDemo.cpp
+++ src/Ch03/03_05e/CodeDemo.cpp
@@ -2,6 +2,45 @@
 // Exercise 03_05
 // Using Several Source Files, by Eduardo Corpeño 
 
+/*
+General notes on Compiling Code:
+- A compiler toolchain is a pipeline of software tools that convert source code into an executable file
+    [source code] --> [compiler toolchain] --> [executable file]
+- inputs can be:
+    1. C++ source files --> compiler translates code to an Assembly file (human-readable version of native language of target CPU); --> Assembly file goes to assembler and produces an object file --> Linker
+    2. Write Assembly code directly (generally a case when you need to write your own peripehral driver) --> assembler to produce an object file --> Linker
+    3. Include libraries for which you only have access to the object files, NOT the source code --> Linker
+Diagram
+|  System   |     Case 1        |     Case 2      |     Case 3      |
+---------------------------------------------------------------------
+| Compiler  |  C++ Source File  |                 |                 |
+|           |        |          |                 |                 |
+|           |        V          |                 |                 |
+| Assembler |   Assembly File   |  Assembly File  |                 |           
+|           |        |          |        |        |                 |
+|           |        V          |        V        |                 |
+| Linker    |    Object File    |   Object File   |   Object File   |
+---------------------------------------------------------------------
+                        \                |                /
+                          \              |              /
+                            \            |            /
+                              \          |          /
+                                \        |        /
+                                  \      |      /
+                                    \    |    /
+                                      \  |  /
+                                        \|/
+                                         V           
+                          [   Executable Binary File   ]
+                             ^^ all handled by IDE ^^
+- C++ source files are 2 components with same name but different extensions:
+    - header file (.h) containing definitions and function definitions (sometimes macros) but NOT executable code
+    - implementation file (.cpp) contain the implementation of all functions declared in header file (i.e. executable code)
+- the compiler must read the header file prior to the implementation file
+- your external code (the one that uses classes) should only include header files (not implementation files)
+- all implementatino files MUST be compiled: must be inluded in the list of source files in the command line, your make file, or the IDE
+*/
+
 #include <iostream>
 #include <string>
 #include "cow.h"
