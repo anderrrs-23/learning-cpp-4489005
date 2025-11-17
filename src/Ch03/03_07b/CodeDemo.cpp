5ee0a784d38ccbff7772bde2573a9dc910873cd3
anderrrs-23 <Andi.Johnson@ngc.com>
2025-11-17 17:54:53 +0000

    added notes for application of dynamic pointers

--- src/Ch03/03_07b/CodeDemo.cpp
+++ src/Ch03/03_07b/CodeDemo.cpp
@@ -6,8 +6,10 @@
 #include <string>
 #include "cow.h"
 
+// below is example for 3_07e that does NOT use object with pointers
 int main(){
-    cow my_cow("Hildy", 7, cow_purpose::pet);
+    cow my_cow("Hildy", 7, cow_purpose::pet); // just decalaring a variable of cow class
+    // accessing cow class members below not using pointers
     std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
     std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
     
--- src/Ch03/03_07e/CodeDemo.cpp
+++ src/Ch03/03_07e/CodeDemo.cpp
@@ -6,14 +6,24 @@
 #include <string>
 #include "cow.h"
 
+/* most important aspect of pointers is dynamic memory management
+- when you declare a variable, you are statically allocating memory for it
+    - compiler knows space/memory needed for it
+    - however, most real applications create variables and objects and memory during runtime = DYNAMIC MEMORY MANAGEMENT
+    - YOU, as a programer, are responsible for managing memory that you allocated in runtime to avoid memory leakage
+    - C++ uses pointers to dynamically allocate memory for new objects when calling the class constructor and also freeing their memory when you no longer need them
+*/
 int main(){
-    cow *my_cow;
+    cow *my_cow; // declaring pointer rather than object
 
-    my_cow = new cow("Gertie", 3, cow_purpose::hide);
+    my_cow = new cow("Gertie", 3, cow_purpose::hide); // "new" operator creates a dynamically allocated instance of the cow class
+    // right-bound arror is short-hand "member of operator" (instead of (*my_cow).get_name())
+    // outline: pointer->member
     std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;
     std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
     
-    delete my_cow;
+    delete my_cow; // deleting after using to clear up memory (opposite of "new" operator)
+    // the delete operator calls the destructor
     
     std::cout << std::endl << std::endl;
     return (0);
